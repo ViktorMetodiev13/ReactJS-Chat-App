d066ddb566310e3b3a3dce1398d1b5a1a87a4937
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-07-13 18:21:58 +0300

    Login and Register changes

--- ReactJS Chat App/src/components/register/Register.jsx
+++ ReactJS Chat App/src/components/register/Register.jsx
@@ -1,16 +1,41 @@
 import "./register.css";
 
+import { useState } from "react";
+
 export const Register = () => {
+    const [avatar, setAvatar] = useState({
+        file: null,
+        url: "",
+    });
+
+    const handleAvatar = (e) => {
+        if (e.target.files[0]) {
+            setAvatar({
+                file: e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0])
+            })
+        }
+    };
+
     return (
         <div className="register">
             <h2 className="login-title">Create an Account</h2>
 
-            <div className="register-avatar">
+            {/* <div className="register-avatar">
                 <img src="./avatar.png" alt="" className="register-avatar-png" />
                 <a href="#">Upload an avatar</a>
-            </div>
+            </div> */}
 
             <form action="POST" >
+                <label htmlFor="file">
+                    <img
+                        src={avatar.url || "./avatar.png"}
+                        alt="avatar png"
+                        className="register-avatar-png"
+                    />
+                    <span className="upload-avatar-text">Upload an avatar</span>
+                </label>
+                <input type="file" id="file" className="register-upload-avatar-field" onChange={handleAvatar} />
                 <input type="text" placeholder='username' className='username' />
                 <input type="text" placeholder='Email' className='email' />
                 <input type="password" placeholder='Password' className='password' />
