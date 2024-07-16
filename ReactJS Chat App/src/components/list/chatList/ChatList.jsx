940ed3a426cd6b23e0b747aed1a4f2d3a9547162
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-07-16 23:46:18 +0300

    changes

--- ReactJS Chat App/src/components/list/chatList/ChatList.jsx
+++ ReactJS Chat App/src/components/list/chatList/ChatList.jsx
@@ -2,7 +2,7 @@ import './chatList.css'
 
 import { useEffect, useState } from 'react'
 
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../../configs/firebase';
 
 import { useUserStore } from '../../../configs/userStore'
@@ -41,7 +41,27 @@ export const ChatList = () => {
     }, [currentUser.id]);
 
     const onSelectedChat = async (chat) => {
-        changeChat(chat.chatId, chat.user);
+        const userChats = chats.map(item => {
+            const { user, ...rest } = item;
+
+            return rest;
+        });
+
+        const chatIndex = userChats.findIndex(item => item.chatId === chat.chatId);
+
+        userChats[chatIndex].isSeen = true;
+
+        const userChatsRef = doc(db, "userchats", currentUser.id);
+
+        try {
+            await updateDoc(userChatsRef, {
+                chats: userChats,
+            });
+            changeChat(chat.chatId, chat.user);
+        } catch (error) {
+            console.log(error);
+        };
+
     };
 
     const showUsersList = () => {
@@ -64,7 +84,14 @@ export const ChatList = () => {
             </div>
 
             {chats.map((chat) => (
-                <div className='users' key={chat.chatId} onClick={() => onSelectedChat(chat)} >
+                <div
+                    className='users'
+                    key={chat.chatId}
+                    onClick={() => onSelectedChat(chat)}
+                    style={{
+                        backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+                    }}
+                >
                     <img src={chat.user.avatar || "./avatar.png"} alt="user image" className="userImage" />
 
                     <div className="userBriefInfo">
